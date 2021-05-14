3de20af71b26f46cb6fc5ca83e65f31620c0e76a
nntp <nonamedtp@gmail.com>
2021-05-14 21:28:33 +0900

    Simple Change

---
+++ ChattingClient/ChildView.h
@@ -0,0 +1,45 @@
+
+// ChildView.h : interface of the CChildView class
+//
+
+
+#pragma once
+
+
+// CChildView window
+
+class CChildView : public CWnd
+{
+// Construction
+public:
+	CChildView();
+
+// Attributes
+public:
+	CArray<TCHAR, TCHAR> m_str;
+	//CPoint *m_caretPos;
+
+	//CRect *m_typingArea;
+// Operations
+public:
+
+// Overrides
+	protected:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+
+// Implementation
+public:
+	virtual ~CChildView();
+
+	// Generated message map functions
+protected:
+	afx_msg void OnPaint();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnSetFocus(CWnd* pOldWnd);
+	afx_msg void OnKillFocus(CWnd* pNewWnd);
+	afx_msg void OnChar(UINT nChar, UINT nRepCnt, UINT nFlags);
+//	afx_msg void OnClose();
+	afx_msg void OnDestroy();
+};
+
