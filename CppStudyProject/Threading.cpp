af704bad01978e1c1422c8e404c30adb9b1f1b66
nntp0 <nonamedtp@gmail.com>
2021-05-31 21:23:25 +0900

    MultiThread 종료조건 설정 연습

--- CppStudyProject/Threading.cpp
+++ CppStudyProject/Threading.cpp
@@ -2,16 +2,33 @@
 #include <iostream>
 #include <thread>
 
-
+bool Threading::finished = false;
 
 void Threading::run() {
-    std::thread t1(counter, 1, 6);
+    std::thread t1(workForFirst);
+    std::thread t2(workForSecond);
 
     t1.join();
+    t2.join();
 }
 
 void counter(int id, int numIterations) {
     for (int i = 0; i < numIterations; i++) {
         std::cout << "Counter " << id << " has value " << i << std::endl;
+    }
+}
+
+void workForFirst() {
+    for (int i = 0; i < 3; i++) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Thread 1";
+    }
+    Threading::finished = true;
+}
+
+void workForSecond() {
+    while(!Threading::finished) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Thread 2";
     }
 }
--- CppStudyProject/Threading.h
+++ CppStudyProject/Threading.h
@@ -1,9 +1,16 @@
 #pragma once
 
 void counter(int, int);
+void workForFirst();
+void workForSecond();
 
 class Threading {
 
+
 public:
 	static void run();
+
+
+public:
+	static bool finished;
 };
