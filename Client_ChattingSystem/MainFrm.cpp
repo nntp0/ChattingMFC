6f8c660386338e702253117118f407b840c4299c
nntp0 <nonamedtp@gmail.com>
2021-06-16 06:20:52 +0900

    클라이언트 종료 시 AfxMessageBox 제거

--- Client_ChattingSystem/MainFrm.cpp
+++ Client_ChattingSystem/MainFrm.cpp
@@ -122,7 +122,6 @@ BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
 void CMainFrame::OnClose()
 {
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
-	AfxMessageBox(_T("Bye"));
 	this->m_transmission->Close();
 	CFrameWnd::OnClose();
 }
